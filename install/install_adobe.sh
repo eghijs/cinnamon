829ebecd4d2e0c141825bc6898208ab2da6553ae
Erik <eghijs@gmail.com>
2018-01-16 20:36:59 -0300

    dd

--- install/install_adobe.sh
+++ install/install_adobe.sh
@@ -6,17 +6,14 @@
 #
 echo
 su root -c "
-echo
-echo "Adcionando repositorio do adobe reader."
 add-apt-repository deb http://archive.canonical.com/ precise partner
 apt-get update -y
 clear
-echo "Repositorio atualizado com sucesso...[ OK ]"
+
 apt-get install adobereader-enu -y
-echo "Adobe Reader instalado com sucesso...[ OK ]"
+
 # Removendo repositorio Adobe Reader
 add-apt-repository -r deb http://archive.canonical.com/ precise partner
-echo "Repositorio Adobe removido...[ OK ]"
 "
 sleep 5
 #exit
