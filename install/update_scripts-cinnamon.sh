c798de75e6c1874873d83658e26b59c3e6ad9c9f
Erik <eghijs@gmail.com>
2018-01-15 23:09:10 -0300

    u

--- install/update_scripts-cinnamon.sh
+++ install/update_scripts-cinnamon.sh
@@ -3,12 +3,12 @@
 ###  BAIXANDO PACOTES DE SCRIPTS  ###
 #####################################
 
-cd $HOME/Downloads
+cd /home/administrador/Downloads
 wget https://github.com/eghijs/cinnamon/archive/master.zip
 unzip master.zip
 mv cinnamon-master/ cinnamon-scripts/
 rm master.zip
-echo "Scripts baixados com sucesso...[OK]"
+echo "Scripts baixado com sucesso...[OK]"
 
 find . -regextype posix-egrep -iregex '.*\.(sh)' -exec chmod 755 {} \;
 echo "Scripts configurado para execucao...[OK]"
