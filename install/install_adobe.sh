e1330073cf8b1802d4597f0e847aaba781f071bc
Erik <eghijs@gmail.com>
2018-01-16 21:26:48 -0300

    ccc

--- install/install_adobe.sh
+++ install/install_adobe.sh
@@ -7,7 +7,7 @@ export PATH
 #####################################
 DIR=/home/$USER/Downloads
 
-wget -c --quiet --show-progress ftp://ftp.adobe.com/pub/adobe/reader/unix/9.x/9.5.5/enu/AdbeRdr9.5.5-1_i386linux_enu.deb -P $DIR
+wget -c --quiet --show-progress https://ardownload.adobe.com/pub/adobe/reader/unix/9.x/9.5.3/enu/AdbeRdr9.5.3-1_i386linux_enu.deb -P $DIR
 echo
 su root -c "
 gdebi Adbe*.deb -y
